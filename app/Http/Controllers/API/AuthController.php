9cc24f485663ad5bcea725665b985a59429c8996
Arno Hanekom <ajhanekom@gmail.com>
2025-09-27 12:55:30 +0200

    Apis Working AUTH now included

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,131 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class AuthController extends Controller
 {
-    //
+    //Register API
+ 
+        public function register(Request $request)
+    {
+
+        //Validate
+        $validator = Validator::make($request->all(), 
+        [
+            'name'      => 'required',
+            'email'     => 'required|unique:users,email',
+            'password'  => 'required|min:8|confirmed',
+            'password_confirmation' => 'required|min:8',
+            
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json( [
+            "status"    => "fail",
+            "message"   => $validator-> errors()
+        ], 400);
+
+        }
+            $data = $request->all();
+
+        //Upload user image (thumbnail)
+        $imagepath = null;
+        if($request->hasFile('profile_picture') && $request->file('profile_picture')-> isValid()){
+
+         $file = $request->file('profile_picture');
+
+         //Generate a unique filename
+
+         $fileName=time().'_'.$file-> getClientOriginalName();
+
+        //Move file to public directory 
+
+        $file->move(public_path('storage/profile'), $fileName);
+
+        //Save the relative path to the database
+
+        $imagePath = "storage/profile". $fileName;
+
+        }
+        $data['profile_picture'] = $imagePath;
+        
+        
+
+        //Create new users Table record
+
+        User::create($data);  
+
+        return response()->json(
+        [
+            "status" => "success",
+            "message"=> "New User has been created successfully"
+        ], 201);
+        
+    }
+            public function login(Request $request)
+{
+    // Validation
+    $validator = Validator::make($request->all(), [
+        'email'    => 'required|email',
+        'password' => 'required|min:8',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            "status"  => "fail",
+            "message" => $validator->errors()
+        ], 400);
+    }
+
+    // Attempt to authenticate
+    if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        $user = Auth::user();
+
+        $response['token'] = $user->createToken('BlogApp')->plainTextToken;
+        $response['email'] = $user->email;
+        $response['name']  = $user->name;
+
+        return response()->json([
+            "status"  => "success",
+            "message" => "User logged in successfully",
+            "data"    => $response
+        ], 200);
+    } else {
+        return response()->json([
+            "status"  => "fail",
+            "message" => "Invalid credentials"
+        ], 401);
+    }
+}
+//Profile of logged in user API
+
+    public function profile()
+{
+    $user = Auth::user();
+
+    return response()->json([
+        "status" => "success",
+        "data"   => $user
+    ], 200);
+}
+
+//Logout API
+    public function logout()
+{
+    $user = Auth::user();
+    $user->tokens()->delete();
+
+        return response()->json([
+        "status" => "success",
+        "message"   => "User logged out successfully"
+    ], 200);
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\TestApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\StudentApiController;
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -21,3 +23,17 @@ use App\Http\Controllers\API\StudentApiController;
 // });
 
 Route::apiResource('/students', StudentApiController::class);
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+
+
+
+
